9f72f336f2a43278efa8e15325bf8b432959c15a
MagmaChocolate <magcho@magcho.local>
2017-01-04 00:31:32 +0900

    [add][update]ズーム機能の実装、UI上部にヘッダーバーを作ることを決定(UIはdrow.ioを参考にする)

--- resource/main2.js
+++ resource/main2.js
@@ -1,62 +1,99 @@
-var canvas = new fabric.Canvas('main');
+window.onload = function(){
+  var canvas = new fabric.Canvas('main');
 
-// ###################################
-// 現在のオブジェクトの数の管理
-var nowToutaiCount = {
-  'totu':0,
-  'fure':0,
-  'par':0
-};
+  // ズーム関連
+  // ズームボタン
+  $('.h-zoom-p').click(function(){
+    canvas.setZoom(canvas.getZoom() * 1.1 ) ;
+  });
+  $('.h-zoom-m').click(function(){
+    canvas.setZoom(canvas.getZoom() / 1.1 ) ;
+  });
+  // ホイールイベント
+  // https://w3g.jp/blog/wheelevent_crossbrowser
+  var mousewheelevent = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+  var mouse_pos = {x:0,y:0};
+  $(document).on(mousewheelevent,function(e){
+    var delta = e.originalEvent.deltaY ? -(e.originalEvent.deltaY) : e.originalEvent.wheelDelta ? e.originalEvent.wheelDelta : -(e.originalEvent.detail);
+    canvas.observe('mouse:move',function(e){
+      mouse_pos.y = e.e.layerY;
+      mouse_pos.x = e.e.layerX;
+      console.log(mouse_pos.x+','+mouse_pos.y);
+    });
+    if (delta < 0){
+      e.preventDefault();
+      var zoomPoint = new fabric.Point(mouse_pos.x, mouse_pos.y);
+      canvas.zoomToPoint(zoomPoint, canvas.getZoom() * 1.1 )
+    } else if (delta > 0){
+      e.preventDefault();
+      var zoomPoint = new fabric.Point(mouse_pos.x, mouse_pos.y);
+      canvas.zoomToPoint(zoomPoint, canvas.getZoom() / 1.1 )
+    }
+  });
+  // プルダウンメニュー
+  var zoomPercent = 100
+  $('.h-zoom-pull-value').change(function() {
+    zoomPercent = $('.h-zoom-pull-value option:selected').attr("value");
+    canvas.setZoom(zoomPercent/100);
+    console.log(zoomPercent);
+  }).change();
 
 
-// 現在追加モードで選択している灯体の種類
-// 灯体の種類
-//  凸1000w         totu
-//  フレネル1000w    fure
-//  パー            par
-//  カーソル        cursor
-var activeToutai = 'cursor';
+  $("#totu").click(function() {
 
+    var mouse_pos = {
+      x: 0,
+      y: 0
+    };
+    canvas.observe('mouse:move',function(e){
+      mouse_pos.y = e.e.layerY;
+      mouse_pos.x = e.e.layerX;
+      console.log(mouse_pos.x+','+mouse_pos.y);
+    });
 
-
-
-$('#main').on('click',function(){
-  /**
-   * 灯体をカーソル位置に追加する関数
-   * @param   {char}   type 追加する灯体の種類の指定、
-   * @return  {bool}
-   */
-  (function(){
-    var mouse_pos = {x:0, y:0};
+    canvas.isDrawingMode = false;
 
     canvas.observe('mouse:down', function(e) {
-      mouse_pos = canvas.getPointer(e.e);
-      console.log(hoge);
-
-      switch (hoge) {
-        case 'totu':
-          var circle = new fabric.Circle({
-            radius:8,
-            left:0,
-            top:0,
-            fill: 'black'
-          });
-          var circle2 = new fabric.Circle({
-            radius:6,
-            left:2,
-            top:2,
-            fill: 'white'
-          });
-
-          canvas.add(new fabric.Group([circle,circle2],{
-            top:mouse_pos.x,
-            left:mouse_pos.y
-          }));
-          console.log('hoge');
-          break;
-      };
-
-      //canvas.off('mouse:down');
-    })();
+      var circle1 = new fabric.Circle({
+        radius:8,
+        left:0,
+        top:0,
+        fill: 'black'
+      });
+      var circle2 = new fabric.Circle({
+        radius:6,
+        left:2,
+        top:2,
+        fill: 'white'
+      });
+      canvas.add(new fabric.Group([circle1,circle2],{
+        left: mouse_pos.x,
+        top: mouse_pos.y
+      }));
+      canvas.off('mouse:move');
+      canvas.off('mouse:down'); // イベントを解除
+    });
   });
-}();
+
+  $('#fure').click(function(){
+
+    var mouse_pos = {
+      x: 0,
+      y: 0
+    };
+    canvas.isDrawingMode = false;
+
+    canvas.observe('mousedown',function(e){
+      mouse_pos = canvas.getPointer(e.e);
+
+      var image;
+      fabric.Image.fromURL('./plan-origin.png', function(img) {
+        image = img.scale(1.0).set({ left: 0, top: 0 });
+        canvas.add(image);
+        canvas.renderAll();
+      });
+
+      canvas.off('mousedown');
+    });
+  });
+}
--- sample/index.js
+++ sample/index.js
@@ -1,295 +1,333 @@
 window.onload = function() {
 
-var canvas = new fabric.Canvas('canvas');
+  var canvas = new fabric.Canvas('canvas');
 
-jQuery(document).ready( function() {
-$("#text_input").click(function(){
+  jQuery(document).ready(function() {
+    $("#text_input").click(function() {
 
-    canvas.isDrawingMode = false;
+      canvas.isDrawingMode = false;
 
-    if (canvas.getContext) {
+      if (canvas.getContext) {
         var context = canvas.getContext('2d');
-    }
+      }
 
-    var text, size, color;
+      var text, size, color;
 
-    var mouse_pos = { x:0 , y:0 };
+      var mouse_pos = {
+        x: 0,
+        y: 0
+      };
 
-    text = $('#text').val();
-    size = $('#size').val();
-    color = $('#color').val();
+      text = $('#text').val();
+      size = $('#size').val();
+      color = $('#color').val();
 
-        canvas.observe('mouse:down', function(e) {
+      canvas.observe('mouse:down', function(e) {
 
         mouse_pos = canvas.getPointer(e.e);
         size = parseInt(size);
 
         canvas.add(new fabric.Text(text, {
-            fontFamily: 'Arial',
-            fontSize: size,
-            left: mouse_pos.x,
-            top: mouse_pos.y,
-            textAlign: "left",
-            strokeWidth: 1,
-            fontWeight: 'bold',
-            strokeStyle: '#000000',
-            fill: color
+          fontFamily: 'Arial',
+          fontSize: size,
+          left: mouse_pos.x,
+          top: mouse_pos.y,
+          textAlign: "left",
+          strokeWidth: 1,
+          fontWeight: 'bold',
+          strokeStyle: '#000000',
+          fill: color
         }));
         canvas.off('mouse:down');
         canvas.renderAll();
         canvas.calcOffset();
-     });
+      });
 
-});
-$("#draw").click(function(){
-    canvas.isDrawingMode = true;
-    canvas.freeDrawingLineWidth = 5;
-    canvas.renderAll();
-    canvas.calcOffset();
-});
-$("#rect").click(function(){
+    });
+    $("#draw").click(function() {
+      canvas.isDrawingMode = true;
+      canvas.freeDrawingLineWidth = 5;
+      canvas.renderAll();
+      canvas.calcOffset();
+    });
+    $("#rect").click(function() {
 
-  var mouse_pos = { x:0 , y:0 };
+      var mouse_pos = {
+        x: 0,
+        y: 0
+      };
 
-  canvas.isDrawingMode = false;
+      canvas.isDrawingMode = false;
 
-  canvas.observe('mouse:down', function(e) {
+      canvas.observe('mouse:down', function(e) {
 
-  mouse_pos = canvas.getPointer(e.e);
+        mouse_pos = canvas.getPointer(e.e);
 
-  canvas.add(new fabric.Rect({
-    left: mouse_pos.x,
-    top: mouse_pos.y,
-    width: 75,
-    height: 50,
-    fill: 'white',
-    stroke: 'black',
-    strokeWidth: 3,
-    padding: 10
-  }));
+        canvas.add(new fabric.Rect({
+          left: mouse_pos.x,
+          top: mouse_pos.y,
+          width: 75,
+          height: 50,
+          fill: 'white',
+          stroke: 'black',
+          strokeWidth: 3,
+          padding: 10
+        }));
 
-  canvas.off('mouse:down');
+        canvas.off('mouse:down');
 
-  });
+      });
 
-});
-$("#circle").click(function(){
+    });
+    $("#circle").click(function() {
 
-  var mouse_pos = { x:0 , y:0 };
+      var mouse_pos = {
+        x: 0,
+        y: 0
+      };
 
-  canvas.isDrawingMode = false;
+      canvas.isDrawingMode = false;
 
-  canvas.observe('mouse:down', function(e) {
+      canvas.observe('mouse:down', function(e) {
 
-  mouse_pos = canvas.getPointer(e.e);
+        mouse_pos = canvas.getPointer(e.e);
 
-  canvas.add(new fabric.Circle({
-    left: mouse_pos.x,
-    top: mouse_pos.y,
-    radius: 30,
-    fill: 'white',
-    stroke: 'black',
-    strokeWidth: 3
-  }));
+        canvas.add(new fabric.Circle({
+          left: mouse_pos.x,
+          top: mouse_pos.y,
+          radius: 30,
+          fill: 'white',
+          stroke: 'black',
+          strokeWidth: 3
+        }));
 
-  canvas.off('mouse:down');
+        canvas.off('mouse:down');
 
-  });
+      });
 
-});
-$("#ellipse").click(function(){
+    });
+    $("#ellipse").click(function() {
 
-  var mouse_pos = { x:0 , y:0 };
+      var mouse_pos = {
+        x: 0,
+        y: 0
+      };
 
-  canvas.isDrawingMode = false;
+      canvas.isDrawingMode = false;
 
-  canvas.observe('mouse:down', function(e) {
+      canvas.observe('mouse:down', function(e) {
 
-  mouse_pos = canvas.getPointer(e.e);
+        mouse_pos = canvas.getPointer(e.e);
 
-  canvas.add(new fabric.Ellipse({
-    rx: 45,
-    ry: 25,
-    fill: 'white',
-    stroke: 'black',
-    strokeWidth: 8,
-    left: mouse_pos.x,
-    top: mouse_pos.y
-  }));
+        canvas.add(new fabric.Ellipse({
+          rx: 45,
+          ry: 25,
+          fill: 'white',
+          stroke: 'black',
+          strokeWidth: 8,
+          left: mouse_pos.x,
+          top: mouse_pos.y
+        }));
 
-  canvas.off('mouse:down');
+        canvas.off('mouse:down');
 
-  });
+      });
 
-});
-$("#line").click(function(){
+    });
+    $("#line").click(function() {
 
-canvas.isDrawingMode = false;
+      canvas.isDrawingMode = false;
 
-if (canvas.getContext) {
-    var context = canvas.getContext('2d');
-}
+      if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+      }
 
-canvas.observe('mouse:down', function(e) { mousedown(e); });
-canvas.observe('mouse:move', function(e) { mousemove(e); });
-canvas.observe('mouse:up', function(e) { mouseup(e); });
+      canvas.observe('mouse:down', function(e) {
+        mousedown(e);
+      });
+      canvas.observe('mouse:move', function(e) {
+        mousemove(e);
+      });
+      canvas.observe('mouse:up', function(e) {
+        mouseup(e);
+      });
 
-var started = false;
-var startX = 0;
-var startY = 0;
+      var started = false;
+      var startX = 0;
+      var startY = 0;
 
-/* Mousedown */
-function mousedown(e) {
-    var mouse = canvas.getPointer(e.e);
-    started = true;
-    startX = mouse.x;
-    startY = mouse.y;
-    canvas.off('mouse:down');
-}
-
-/* Mousemove */
-function mousemove(e) {
-
-    if(!started) {
-
-        return false;
-
-    }
-    canvas.off('mouse:move');
-
-}
-
-/* Mouseup */
-function mouseup(e) {
-
-    if(started) {
-
+      /* Mousedown */
+      function mousedown(e) {
         var mouse = canvas.getPointer(e.e);
+        started = true;
+        startX = mouse.x;
+        startY = mouse.y;
+        canvas.off('mouse:down');
+      }
 
-        canvas.add(new fabric.Line([startX, startY, mouse.x, mouse.y],{ stroke: "#000000", strokeWidth: 2}));
-        canvas.renderAll();
-        canvas.calcOffset(); 
+      /* Mousemove */
+      function mousemove(e) {
 
-        started = false;
-        canvas.off('mouse:up');
+        if (!started) {
 
-    }   
+          return false;
 
- }
+        }
+        canvas.off('mouse:move');
 
-});
-$("#save").click(function(){
-    canvas.isDrawingMode = false;
-    if(!window.localStorage){alert("画像保存機能はブラウザがサポートしていません。"); return;}
-    // save to localStorage
-    var json = JSON.stringify(canvas);
-    window.localStorage.setItem("hoge", json);
-});
-$("#load").click(function(){
-    canvas.isDrawingMode = false;
-    if(!window.localStorage){alert("画像保存機能はブラウザがサポートしていません。"); return;}
-    //clear canvas
-    canvas.clear();
-    //load from localStorage
-    canvas.loadFromJSON(window.localStorage.getItem("hoge"));
-    // re-render the canvas
-    canvas.renderAll();
-    // optional
-    canvas.calcOffset();
-});
-$("#delete").click(function(){
-    canvas.isDrawingMode = false;
-    if(!window.localStorage){alert("画像保存機能はブラウザがサポートしていません。"); return;}
-    if (confirm('本当に削除しますか？')) {
+      }
+
+      /* Mouseup */
+      function mouseup(e) {
+
+        if (started) {
+
+          var mouse = canvas.getPointer(e.e);
+
+          canvas.add(new fabric.Line([startX, startY, mouse.x, mouse.y], {
+            stroke: "#000000",
+            strokeWidth: 2
+          }));
+          canvas.renderAll();
+          canvas.calcOffset();
+
+          started = false;
+          canvas.off('mouse:up');
+
+        }
+
+      }
+
+    });
+    $("#save").click(function() {
+      canvas.isDrawingMode = false;
+      if (!window.localStorage) {
+        alert("画像保存機能はブラウザがサポートしていません。");
+        return;
+      }
+      // save to localStorage
+      var json = JSON.stringify(canvas);
+      window.localStorage.setItem("hoge", json);
+    });
+    $("#load").click(function() {
+      canvas.isDrawingMode = false;
+      if (!window.localStorage) {
+        alert("画像保存機能はブラウザがサポートしていません。");
+        return;
+      }
+      //clear canvas
+      canvas.clear();
+      //load from localStorage
+      canvas.loadFromJSON(window.localStorage.getItem("hoge"));
+      // re-render the canvas
+      canvas.renderAll();
+      // optional
+      canvas.calcOffset();
+    });
+    $("#delete").click(function() {
+      canvas.isDrawingMode = false;
+      if (!window.localStorage) {
+        alert("画像保存機能はブラウザがサポートしていません。");
+        return;
+      }
+      if (confirm('本当に削除しますか？')) {
         window.localStorage.removeItem("hoge");
-    }
-});
-$("#clear").click(function(){
-    canvas.isDrawingMode = false;
-    if (confirm('本当にクリアしますか？')) {
+      }
+    });
+    $("#clear").click(function() {
+      canvas.isDrawingMode = false;
+      if (confirm('本当にクリアしますか？')) {
         canvas.clear();
-    }
-});
-$("#remove").click(function(){
-    canvas.isDrawingMode = false;
+      }
+    });
+    $("#remove").click(function() {
+      canvas.isDrawingMode = false;
 
-    var activeObject = canvas.getActiveObject(),
-    activeGroup = canvas.getActiveGroup();
-    if (activeObject) {
+      var activeObject = canvas.getActiveObject(),
+        activeGroup = canvas.getActiveGroup();
+      if (activeObject) {
         if (confirm('本当に削除しますか？')) {
-            canvas.remove(activeObject);
+          canvas.remove(activeObject);
         }
-    }
-    else if (activeGroup) {
+      } else if (activeGroup) {
         if (confirm('本当に削除しますか？')) {
-            var objectsInGroup = activeGroup.getObjects();
-            canvas.discardActiveGroup();
-            objectsInGroup.forEach(function(object) {
+          var objectsInGroup = activeGroup.getObjects();
+          canvas.discardActiveGroup();
+          objectsInGroup.forEach(function(object) {
             canvas.remove(object);
-            });
+          });
         }
-    }
+      }
 
-});
-$("#image_save").click(function(){
-    canvas.isDrawingMode = false;
-    if(!window.localStorage){alert("画像保存機能はブラウザがサポートしていません。"); return;}
-    // save to localStorage
-    var base64 = $('canvas').get(0).toDataURL('png');
-    window.localStorage.setItem("foo", base64);
-});
-$("#image_load").click(function(){
-    canvas.isDrawingMode = false;
-    if(!window.localStorage){alert("画像保存機能はブラウザがサポートしていません。"); return;}
-    //load from localStorage
-    var base64 = window.localStorage.getItem("foo");
-    if (base64) {
-                if (canvas.getContext) {
-                    var context = canvas.getContext('2d');
-                }
-                canvas.clear();
-                var image = new Image();
-                image.onload = function() {
-                    fabric.Image.fromURL(image.src, function(img) {
-                        canvas.add(img);
-                        img.set('originX', 350);
-                        img.set('originY', 300);
-                        img.set('left', 350);
-                        img.set('top', 300);
-                        img.set('zindex', 0);
-                        img.set('selectable', false);
-                        canvas.bringToFront(img);
-                    });
-                canvas.renderAll();
-                canvas.calcOffset();       
-                };
-                    /*
-                    location.href = image.src;
-                    context.drawImage(image, 0, 0);
-                    var data = base64.replace(/^data:image\/(png|jpg);base64,/, "");
-                    */
+    });
+    $("#image_save").click(function() {
+      canvas.isDrawingMode = false;
+      if (!window.localStorage) {
+        alert("画像保存機能はブラウザがサポートしていません。");
+        return;
+      }
+      // save to localStorage
+      var base64 = $('canvas').get(0).toDataURL('png');
+      window.localStorage.setItem("foo", base64);
+    });
+    $("#image_load").click(function() {
+      canvas.isDrawingMode = false;
+      if (!window.localStorage) {
+        alert("画像保存機能はブラウザがサポートしていません。");
+        return;
+      }
+      //load from localStorage
+      var base64 = window.localStorage.getItem("foo");
+      if (base64) {
+        if (canvas.getContext) {
+          var context = canvas.getContext('2d');
+        }
+        canvas.clear();
+        var image = new Image();
+        image.onload = function() {
+          fabric.Image.fromURL(image.src, function(img) {
+            canvas.add(img);
+            img.set('originX', 350);
+            img.set('originY', 300);
+            img.set('left', 350);
+            img.set('top', 300);
+            img.set('zindex', 0);
+            img.set('selectable', false);
+            canvas.bringToFront(img);
+          });
+          canvas.renderAll();
+          canvas.calcOffset();
+        };
+        /*
+        location.href = image.src;
+        context.drawImage(image, 0, 0);
+        var data = base64.replace(/^data:image\/(png|jpg);base64,/, "");
+        */
 
-                image.src = base64;
-    }
-});
-$("#image_delete").click(function(){
-    canvas.isDrawingMode = false;
-    if(!window.localStorage){alert("画像保存機能はブラウザがサポートしていません。"); return;}
-    if (confirm('本当に削除しますか？')) {
+        image.src = base64;
+      }
+    });
+    $("#image_delete").click(function() {
+      canvas.isDrawingMode = false;
+      if (!window.localStorage) {
+        alert("画像保存機能はブラウザがサポートしていません。");
+        return;
+      }
+      if (confirm('本当に削除しますか？')) {
         window.localStorage.removeItem("foo");
-    }
-});
+      }
+    });
 
-});
+  });
 
-canvas.calcOffset();
-
-document.onkeyup = function(e) {
-  canvas.renderAll();
-};
-
-setTimeout(function() {
   canvas.calcOffset();
-}, 100);
+
+  document.onkeyup = function(e) {
+    canvas.renderAll();
+  };
+
+  setTimeout(function() {
+    canvas.calcOffset();
+  }, 100);
 
 };
