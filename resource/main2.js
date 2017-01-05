e2474ee7327f42aa38712908c474d6d9e5276ab4
MagmaChocolate <magcho@magcho.local>
2017-01-06 01:37:01 +0900

    [add][update]右クリックメニューの実装、コピペ周りを要検証。背景を追加した、灯体はSVGで置くことに変更

--- resource/main2.js
+++ resource/main2.js
@@ -8,6 +8,20 @@ window.onload = function(){
               fure500   フレネル500W灯体描画モード
 
     */
+    ctrlState = false;
+    $(window).keydown(function(e){
+      if(e.keyCode == 17 || e.keyCode == 91){
+        ctrlState = true; //Ctrlキー 17,commandキー91
+      }
+    });
+    $(window).keyup(function(e){
+      if(e.keyCode == 17 || e.keyCode == 91){
+        ctrlState = false; //Ctrlキー 17,commandキー91
+      }
+    });
+    /*
+      ctrlState  trueは押下、falseは押してない
+    */
     canvas = new fabric.Canvas('main'); // mainはID名のみ有効（class名は不可)
 
     canvas.observe('mouse:move', function(e) {
@@ -29,7 +43,7 @@ window.onload = function(){
     });
     imgInstance.shadow = new fabric.Shadow({
       color:'#696969',
-      blur:30
+      //blur:30
     })
     canvas.add(imgInstance);
   })();
@@ -48,23 +62,24 @@ function startProgram(){
   // ホイールイベント
   // https://w3g.jp/blog/wheelevent_crossbrowser
   var mousewheelevent = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  var mouse_pos = {
-    x: 0,
-    y: 0
-  };
   $(document).on(mousewheelevent, function(e) {
-    var delta = e.originalEvent.deltaY ? -(e.originalEvent.deltaY) : e.originalEvent.wheelDelta ? e.originalEvent.wheelDelta : -(e.originalEvent.detail);
-    canvas.observe('mouse:move', function(e) {
-      mouse_pos = canvas.getPointer(e.e);
-    });
-    if (delta < 0) {
-      e.preventDefault();
-      canvas.zoomToPoint(new fabric.Point(mouse_pos.x, mouse_pos.y), canvas.getZoom() * 1.1)
-    } else if (delta > 0) {
-      e.preventDefault();
-      canvas.zoomToPoint(new fabric.Point(mouse_pos.x, mouse_pos.y), canvas.getZoom() / 1.1)
+    if (ctrlState){
+      var delta = e.originalEvent.deltaY ? -(e.originalEvent.deltaY) : e.originalEvent.wheelDelta ? e.originalEvent.wheelDelta : -(e.originalEvent.detail);
+      canvas.observe('mouse:move', function(e) {
+        var mouse_pos = canvas.getPointer(e.e);
+      });
+      if (delta < 0) {
+        e.preventDefault();
+        canvas.zoomToPoint(new fabric.Point(mouse_pos.x, mouse_pos.y), canvas.getZoom() * 1.1)
+      } else if (delta > 0) {
+        e.preventDefault();
+        canvas.zoomToPoint(new fabric.Point(mouse_pos.x, mouse_pos.y), canvas.getZoom() / 1.1)
+      }
     }
   });
+
+
+
   // プルダウンメニュー
   var zoomPercent = 100
   $('.h-zoom-pull-value').change(function() {
@@ -77,33 +92,14 @@ function startProgram(){
 
 
  /* 灯体描画 */
-  canvas.observe('mouse:down',function(){
-    switch (nowState) {
-      case 'cursor':
-
-        break;
-      default:
-
-    }
-  });
 
 
 
   // 凸1000wの描画
   $(".totu").click(function() {
-
-    var mouse_pos = {
-      x: 0,
-      y: 0
-    };
-    canvas.observe('mouse:move', function(e) {
-      mouse_pos = canvas.getPointer(e.e)
-      //console.log(mouse_pos.x + ',' + mouse_pos.y);
-    });
-
     canvas.isDrawingMode = false;
-
     canvas.observe('mouse:down', function(e) {
+      var mouse_pos = canvas.getPointer(e.e);
       var circle1 = new fabric.Circle({
         radius: 8,
         left: 0,
@@ -120,35 +116,25 @@ function startProgram(){
         left: mouse_pos.x,
         top: mouse_pos.y
       }));
-      canvas.off('mouse:move');
+      console.log(mouse_pos);
       canvas.off('mouse:down'); // イベントを解除
     });
   });
-
-  $('.fure').click(function() {
-
-    var mouse_pos = {
-      x: 0,
-      y: 0
-    };
-    canvas.isDrawingMode = false;
-
-    canvas.observe('mousedown', function(e) {
-      mouse_pos = canvas.getPointer(e.e);
-
-      var image;
-      fabric.Image.fromURL('./plan-origin.png', function(img) {
-        image = img.scale(1.0).set({
-          left: 0,
-          top: 0
-        });
-        canvas.add(image);
+  $('.fure').click(function(){
+    //http://www.independent-software.com/loading-an-svg-image-with-fabric-js/
+      canvas.observe('mouse:down',function(e){
+        var mouse_pos = canvas.getPointer(e.e);
+        fabric.loadSVGFromURL('./resource/svg/totu.svg',function(objects,options){
+        var svgObject = fabric.util.groupSVGElements(objects,options);
+        svgObject.top = mouse_pos.y;
+        svgObject.left = mouse_pos.x;
+        canvas.add(svgObject);
         canvas.renderAll();
+        canvas.off('mouse:down');
       });
-
-      canvas.off('mousedown');
     });
   });
+
 
 
 
@@ -161,7 +147,7 @@ function startProgram(){
       canvas.remove(activeObject);
     } else if (activeGroup) {
       var objectsInGroup = activeGroup.getObjects();
-      canvas.discardActiveGroup();
+      canvas.discardActiveGroup(); //選択状態にあるグループのイベントを外す
       objectsInGroup.forEach(function(object) {
         canvas.remove(object);
       });
@@ -173,23 +159,94 @@ function startProgram(){
 
 
  /*右クリックメニュー*/
-  $('canvas').bind('contextmenu',function(){
-    $('.right-click-menu').css('display','block'); //右クリックメニューの表示
+  $('canvas').bind('contextmenu',function(e){
+    displayRightMenu('show',e); //右クリックメニューの表示
     canvas.observe('mouse:down',function(){
-      $('.right-click-menu').css('display','none'); //右クリックメニューを隠す
+      displayRightMenu('hide'); //右クリックメニューを隠す
       canvas.off('mouse:down');
     });
     return false
   });
-  function deleteRightMenu(){
-    $('.right-click-menu').css('display','none');
+  function displayRightMenu(mode,e){
+    if (mode == 'show'){
+      var rc_mouse_pos = {x:e.clientX,y:e.clientY};
+      $('.right-click-menu').css({
+        'display':'block',
+        'left':rc_mouse_pos.x,
+        'top':rc_mouse_pos.y
+      });
+    }else if (mode == 'hide') {
+      $('.right-click-menu').css('display','none');
+    }
   }
+  //コピー
+  $('.rc-copy').click(function(){
+    // http://stackoverflow.com/questions/37192881/fabricjs-clipboard-implementation-copy-paste
+    // Single Object
+    if(canvas.getActiveObject()) {
+        // Does this object require an async clone?
+        if(!fabric.util.getKlass(canvas.getActiveObject().type).async) {
+            clipboard = canvas.getActiveObject().clone();
+        } else {
+            canvas.getActiveObject().clone(function(clone) {
+                clipboard= clone;
+            });
+        }
+    }
 
+    // Group of Objects (all groups require async clone)
+    if(canvas.getActiveGroup()) {
+        canvas.getActiveGroup().clone(function(clone) {
+            clipboard = clone;
+        });
+    }
+    displayRightMenu('hide');
+  });
+  //ペースト
+  $('.rc-paste').click(function(e){
+    // http://stackoverflow.com/questions/37192881/fabricjs-clipboard-implementation-copy-paste
+    // Do we have an object in our clipboard?
+    if(clipboard) {
+        // Lets see if we need to clone async
+        if(!fabric.util.getKlass(clipboard.type).async) {
+            var obj = clipboard.clone();
+            obj.setTop(obj.top += 10);
+            obj.setLeft(obj.left += 10);
+            canvas.add(obj);
+            // We do not need to clone async, all groups require async clone
+            canvas.setActiveObject(obj);
+            clipboard = obj;
+        }  else {
+            clipboard.clone(function(clone) {
+                // clone.setTop(clone.top += 10);
+                // clone.setLeft(clone.left += 10);
+                var mouse_pos = canvas.getPointer(e.e)
+                clone.setTop(mouse_pos.y);
+                clone.setLeft(mouse_pos.x);
 
+                clone.forEachObject(function(obj){
+										canvas.add(obj);
+								});
 
+   							canvas.deactivateAll();
+
+                // We need to clone async, but this doesnt mean its a group
+                if(clipboard.isType("group")) {
+                    canvas.setActiveGroup(clone);
+                } else {
+                    canvas.setActiveObject(clone);
+                }
+                clipboard = clone;
+            });
+        }
+    }
+    canvas.renderAll();
+    displayRightMenu('hide');
+  });
+  //削除
   $('.rc-delete').click(function(){
     deleteObjects();
-    deleteRightMenu();
+    displayRightMenu('hide');
   });
 
 }; //startProgramの閉じ
